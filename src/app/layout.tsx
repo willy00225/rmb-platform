f70541c2456961127bb0073d3a9444bba6146755
Willy00225 <willyblack369@gmail.com>
2026-06-22 19:32:44 +0000

    ajuster le logo et les paramètres sur mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr" className={`${inter.variable} ${lexend.variable}`} suppressHydrationWarning>
       <head>
+        {/* PWA */}
+        <meta name="application-name" content="RMB Connect" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="RMB Connect" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#005A3A" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+
+        {/* OneSignal */}
         <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer />
         <script
           dangerouslySetInnerHTML={{
